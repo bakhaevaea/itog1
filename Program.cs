44ba2b3e379e861090592b4a494d86abbd466623
Евгения <bakhaeva@gmail.com>
2022-10-03 22:39:44 +0300

    добавлена диаграмма

--- Program.cs
+++ Program.cs
@@ -8,12 +8,27 @@ class MyNewClass
                         "и", "направьте", "файл", "с", "выполненной", "работой", "в", "окно", "загрузки", "«практического", "задания»"};
         
         Console.WriteLine(String.Join("', '", stringArr));
+        
         //массив, содержащий индексы элементов с длиной <=3
         int [] indexArr = new int [stringArr.Length];
+        int count  = 0;  // кол-во элементов с длиной <=3
+        
+        for (int i = 0; i < stringArr.Length; i++ )    
+        {
+            if (stringArr[i].Length <= 3)
+            {
+                indexArr[count] = i;
+                count ++;
+            }
+        }
 
-        // for (int i = 0; i < stringArr.Length; i++ )    
-        // {
-            
-        // }
+        // Формируем результирующий массив
+        string [] stringArrLen3 = new string [count];
+        for (int i = 0; i < count; i++ )
+        {
+            stringArrLen3[i] = stringArr[indexArr[i]];
+        }
+
+        Console.WriteLine(String.Join("', '", stringArrLen3));
     }
 }
