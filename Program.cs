29d021bdb3d5ff664d9829dc9748df85fa7c4cea
Евгения <bakhaeva@gmail.com>
2022-10-03 22:03:08 +0300

    в программе введен исходный массив

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MyNewClass
+{
+    static void Main()
+    {
+        string [] stringArr = {"Скачайте", "файл", "контрольной", "работы", "справа", "от", "урока", ",",  "решите", "задачи",
+                        "и", "направьте", "файл", "с", "выполненной", "работой", "в", "окно", "загрузки", "«практического", "задания»"};
+        
+        Console.WriteLine(String.Join("', '", stringArr));
+        //массив, содержащий индексы элементов с длиной <=3
+        int [] indexArr = new int [stringArr.Length];
+
+        // for (int i = 0; i < stringArr.Length; i++ )    
+        // {
+            
+        // }
+    }
+}
